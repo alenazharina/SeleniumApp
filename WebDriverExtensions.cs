ca0730e34fa1df1ff28d412225aab5bfb3715101
Alena Zharina <alenazharina@coherentsolutions.com>
2022-09-07 13:52:21 +0300

    selenium 3

---
+++ WebDriverExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumExtras.WaitHelpers;
+
+namespace SeleniumSecondApp
+{
+    class WebDriverExtensions
+    {
+        public static IWebElement WaitForElement(this IWebDriver driver, By by, TimeSpan timeoutInSeconds)
+        {
+             var wait = new WebDriverWait(driver)
+        }
+    }
+}
